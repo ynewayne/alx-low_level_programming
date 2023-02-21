4902797f23153d770bd7687ba5c79cbeb4eafc95
Samson Wayne Muhadia <smuhadia4@gmail.com>
2023-02-21 11:02:21 -0800

    corrected 0-putchar.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,7 +1,7 @@
 #ifndef main_h
 #define main_h
 
-int _putcher(char);
+int _putchar(char);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
@@ -12,7 +12,7 @@ int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
 int add(int, int);
-void print_to_98(int);
+void print_to_98(int n);
 void print_times_table(int n);
 
 #endif
