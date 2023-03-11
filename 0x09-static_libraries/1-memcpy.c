9f545177bb81de2a61e0cb2e54dfcd0dce4373e1
Samson Wayne Muhadia <smuhadia4@gmail.com>
2023-03-10 18:42:30 -0800

    created 0x09 C script

---
+++ 0x09-static_libraries/1-memcpy.c
@@ -0,0 +1,61 @@
+#include "main.h"
+/**
+ *_memcpy - a function that copies memory area
+ *@dest: memory where is stored
+ *@src: memory where is copied
+ *@n: number of bytes
+ *
+ *Return: copied memory with n byted changed
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int r = 0;
+	int i = n;
+
+
+	for (; r < i; r++)
+	{
+		dest[r] = src[r];
+		n--;
+	}
+	return (dest);
+}
+ 
+ 
+ 
+============================
+ 
+1-strncat.c
+
+
+#include "main.h"
+/**
+ * _strncat - concatenate two strings
+ * using at most n bytes from src
+ * @dest: input value
+ * @src: input value
+ * @n: input value
+ *
+ * Return: dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i;
+	int j;
+
+
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	j = 0;
+	while (j < n && src[j] != '\0')
+	{
+	dest[i] = src[j];
+	i++;
+	j++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
---
+++ 0x09-static_libraries/README.md
@@ -0,0 +1,1 @@
+s directory contains the scripts for the 0x09 static libraries task
