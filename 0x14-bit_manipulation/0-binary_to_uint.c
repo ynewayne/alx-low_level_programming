1fea713a2bdcedc312bc924116a2397867f466ab
Samson Wayne <smuhadia4@gmail.com>
2023-12-04 03:26:29 -0800

    corrected the file names

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * binary_to_uint - function changes a binary number to an unsigned int.
- * @b: pointer to a string with  a binary number
+ * binary_to_uint - function converts a binary number to an unsigned int.
+ * @b: pointer to a string containing a binary number
  *
  * Return: unsigned int with decimal value of binsry number, or 0 if error
  */
